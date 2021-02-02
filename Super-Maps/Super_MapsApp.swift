4b75df9526e15fbac4793d2749972a23e5aab983
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-02-02 22:26:19 +0100

    Initial Commit

---
+++ Super-Maps/Super_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Super_MapsApp.swift
+//  Super-Maps
+//
+//  Created by Aitor Zubizarreta Perez on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Super_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
